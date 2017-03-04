0818582fecfc5e709e66c713701c0f20e37a49ad
Risto Pulkkinen <risto.pulkkinen@cs.tamk.fi>
2017-03-04 14:38:22 +0200

    Initial commit

---
+++ android/src/fi/tamk/tiko/kivimiesgaming/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package fi.tamk.tiko.kivimiesgaming;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import fi.tamk.tiko.kivimiesgaming.Vescape;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Vescape(), config);
+	}
+}
---
+++ desktop/src/fi/tamk/tiko/kivimiesgaming/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package fi.tamk.tiko.kivimiesgaming.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import fi.tamk.tiko.kivimiesgaming.Vescape;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Vescape(), config);
+	}
+}
