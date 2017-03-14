883120f5e09a798e8ce425c1428c652a5f0cc78a
Atte-Petteri Ronkanen <atte.ronkanen@cs.tamk.fi>
2017-03-14 13:56:14 +0200

    roomicon on select animations

--- core/src/fi/tamk/tiko/kivimiesgaming/RoomButton.java
+++ core/src/fi/tamk/tiko/kivimiesgaming/RoomButton.java
@@ -1,7 +1,9 @@
 package fi.tamk.tiko.kivimiesgaming;
 
+import com.badlogic.gdx.math.Interpolation;
 import com.badlogic.gdx.scenes.scene2d.Group;
 import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 /**
  * Created by atter on 11-Mar-17.
@@ -37,5 +39,23 @@ public class RoomButton extends SelectableButton {
 
     public RoomData getRoomData() {
         return roomData;
+    }
+
+    @Override
+    protected void onSelect(boolean selected) {
+        int direction = selected ? 1 : -1;
+
+        if (selected) {
+            roomIcon.addAction(Actions.parallel(
+                    Actions.moveBy(0, direction * roomIcon.getSizeY(), 0.4f, Interpolation.pow2),
+                    Actions.scaleTo(1.4f, 1.4f, 0.3f)
+            ));
+        } else {
+            roomIcon.addAction(Actions.parallel(
+                    Actions.moveBy(0, direction * roomIcon.getSizeY(), 0.8f, Interpolation.bounceOut),
+                    Actions.scaleTo(1f, 1f, 0.15f)
+            ));
+        }
+
     }
 }
