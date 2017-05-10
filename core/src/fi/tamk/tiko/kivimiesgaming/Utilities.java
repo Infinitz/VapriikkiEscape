55c0c5835f25f7a49637df683de9bd1329265912
Risto Pulkkinen <risto.pulkkinen@cs.tamk.fi>
2017-05-10 11:05:10 +0300

    Documented utilities class

--- core/src/fi/tamk/tiko/kivimiesgaming/Utilities.java
+++ core/src/fi/tamk/tiko/kivimiesgaming/Utilities.java
@@ -2,9 +2,17 @@ package fi.tamk.tiko.kivimiesgaming;
 
 /**
  * @author Atte-Petteri Ronkanen, Risto Pulkkinen
+ *
+ * This class helps to split text into lines.
  */
 public class Utilities {
 
+    /**
+     * Class constructor.
+     * @param str The string that needs to be split.
+     * @param charsPerLine How many characters are allowed for each line.
+     * @return Returns the splitted string.
+     */
     public static String splitTextIntoLines(String str, int charsPerLine) {
         int currentLineStart = 0;
         String splittedString = "";
