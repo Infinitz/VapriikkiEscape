9c8cae71c7171cf922cbd3087f94504d33652b24
Risto Pulkkinen <risto.pulkkinen@cs.tamk.fi>
2017-05-10 12:36:46 +0300

    Added documentatiton

--- core/src/fi/tamk/tiko/kivimiesgaming/TutorialRoomView.java
+++ core/src/fi/tamk/tiko/kivimiesgaming/TutorialRoomView.java
@@ -13,17 +13,43 @@ import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 
 /**
  * @author Atte-Petteri Ronkanen, Risto Pulkkinen
+ *
+ * This class controls the tutorial, which is slightly different than other rooms.
  */
 public class TutorialRoomView extends RoomView {
 
+    /**
+     * Controls how marks are allowed in a line in the text bubble.
+     */
     private final int marksInLineBubble = 21;
+
+    /**
+     * Controls the bubble size.
+     */
     private final float bubbleSize = 700f;
 
+    /**
+     * rossFace texture.
+     */
     protected ImageActor rossFace;
-    protected Group currentBubbleGroup;
-    protected Label.LabelStyle labelStyle;
-    protected boolean ableToProgress;
 
+    /**
+     * Current bubble group consisting of the bubble texture and the text.
+     */
+    protected Group currentBubbleGroup;
+
+    /**
+     * LabelStyle for the text.
+     */
+    protected Label.LabelStyle labelStyle;
+
+
+    /**
+     * Class constructor.
+     * @param game Main class of the game.
+     * @param roomData Data of the room in question
+     * @param assetManager AssetManager for loading the textures.
+     */
     public TutorialRoomView(Vescape game, RoomData roomData, AssetManager assetManager) {
         super(game, roomData, assetManager);
         assetManager.load("story_ross_temp.png", Texture.class);
@@ -31,6 +57,9 @@ public class TutorialRoomView extends RoomView {
         riddlesInRoom = 2;
     }
 
+    /**
+     * Starts the room as in parent class, plus creates the character's face and text style.
+     */
     @Override
     public void onStart() {
         super.onStart();
@@ -45,6 +74,9 @@ public class TutorialRoomView extends RoomView {
                         75);
     }
 
+    /**
+     * Checks which of the 2 riddles is in play and adds the text according to that.
+     */
     @Override
     protected void onNewRiddleStarted() {
         if (currentRiddleCount == 0) {
@@ -54,6 +86,9 @@ public class TutorialRoomView extends RoomView {
         }
     }
 
+    /**
+     * Disposes the screen.
+     */
     @Override
     public void dispose() {
         super.dispose();
@@ -61,6 +96,10 @@ public class TutorialRoomView extends RoomView {
         assetManager.unload("talk_bubble.png");
     }
 
+    /**
+     * Adds talking character to the screen.
+     * @param text The text what the character says.
+     */
     protected void rossTalk(final String text) {
         final ImageActor touchDetector = new ImageActor(
                 assetManager.get("black.png", Texture.class), Vescape.GUI_VIEWPORT_HEIGHT);
@@ -96,6 +135,10 @@ public class TutorialRoomView extends RoomView {
         ));
     }
 
+    /**
+     * Creates new bubble.
+     * @param text The text what the bubble says.
+     */
     protected void newBubble(String text) {
         ImageActor bubble;
         if (text.length() == 0) {
@@ -143,17 +186,6 @@ public class TutorialRoomView extends RoomView {
         } else {
             currentBubbleGroup = bubbleGroup;
         }
-
-        bubbleGroup.addAction(
-                Actions.sequence(
-                        Actions.run(new Runnable() {
-                            @Override
-                            public void run() {
-                                //ableToProgress = true;
-                            }
-                        })
-                )
-        );
 
         if (currentBubbleGroup == null) {
             currentBubbleGroup = bubbleGroup;
